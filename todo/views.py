8a5937d66f3ecf5d9d811f6f1e749d608479b3c9
Anelise Bernardy <anelise.dev@gmail.com>
2026-08-29 22:55:00 -0300

    Implement task update functionality and refactor templates

--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, CreateView, UpdateView
 from django.urls import reverse_lazy
 
 
@@ -8,6 +8,11 @@ class TodoListView(ListView):
     model = Todo
 
 class TodoCreateView(CreateView):
+    model = Todo
+    fields = ['title', 'deadLine']
+    success_url = reverse_lazy('todo_list')
+
+class TodoUpdateView(UpdateView):
     model = Todo
     fields = ['title', 'deadLine']
     success_url = reverse_lazy('todo_list')
