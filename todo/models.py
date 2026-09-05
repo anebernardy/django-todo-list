6ef9c6eb59a63dceba87d34d15c532ec0b2b5ba7
Anelise Bernardy <anelise.dev@gmail.com>
2026-09-05 11:59:59 -0300

    Improve task form and presentation

--- todo/models.py
+++ todo/models.py
@@ -4,21 +4,16 @@ from django.utils import timezone
 
 class Todo(models.Model):
     title = models.CharField(
-        verbose_name="Título", 
-        max_length=100, 
-        null=False, 
-        blank=False,
+        verbose_name="Título",
+        max_length=100,
     )
-    created_at = models.DateTimeField(
-        auto_now_add=True, 
-        null=False, 
-        blank=False,
-    )
+    
+    created_at = models.DateTimeField(auto_now_add=True)
+
     deadline = models.DateField(
-        verbose_name="Data de entrega", 
-        null=False, 
-        blank=False,
+        verbose_name="Data de entrega",
     )
+
     finished_at = models.DateField(
         verbose_name="Data de finalização",
         null=True,
